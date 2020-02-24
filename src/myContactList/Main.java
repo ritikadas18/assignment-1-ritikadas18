2418e85b7342e23465aebf63b873e0800ad3c3c5
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-02-24 22:39:14 +0530

    add choices

--- src/myContactList/Main.java
+++ src/myContactList/Main.java
@@ -9,6 +9,32 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         myLinkedList linkedList1 = new myLinkedList();
         myTasks tasks1 = new myTasks();
+        System.out.println("Welcome to Ritika's contact list application");
+        System.out.println("press 1 to add a new contact");
+        System.out.println("press 2 to view all contacts");
+        System.out.println("press 3 to search for a contact");
+        System.out.println("Press 4 to delete a contact");
+        System.out.println("Press 5 to exit program ");
+        int choice = Integer.parseInt(sc.nextLine());
+        if (choice == 1) {
+            tasks1.addContact(myLinkedList);
+            choice = Integer.parseInt(sc.nextLine());
+        }
+        if (choice == 2) {
+            myTasks.viewContact(myLinkedList);
+            choice = Integer.parseInt(sc.nextLine());
 
+        }
+        if (choice == 3) {
+            myTasks.searchContact(myLinkedList);
+            choice = Integer.parseInt(sc.nextLine());
+        }
+        if (choice == 4) {
+            myTasks.deleteContact(myLinkedList);
+            choice = Integer.parseInt(sc.nextLine());
+        }
+        if (choice == 5) {
+            System.out.println("Application terminate");
+        }
     }
 }
