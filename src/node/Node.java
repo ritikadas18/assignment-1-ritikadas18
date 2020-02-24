261ace9c047941d96f764301b8c15b97eb8ff974
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-02-24 22:57:38 +0530

    init commit

---
+++ src/node/Node.java
@@ -0,0 +1,18 @@
+package node;
+
+import person.myPerson;
+
+public class Node {
+    private myPerson value;
+
+    private Node next;
+
+
+    public Node(myPerson value) {
+
+        this.value = value;
+
+    }
+
+
+}
