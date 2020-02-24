00c72e66758d77b38997f302e1e88dabcae1ceaf
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-02-24 22:36:46 +0530

    init commit

---
+++ src/myContactList/Main.java
@@ -0,0 +1,14 @@
+package myContactList;
+
+import myLinkedListOperations.myLinkedList;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        myLinkedList linkedList1 = new myLinkedList();
+        myTasks tasks1 = new myTasks();
+
+    }
+}
