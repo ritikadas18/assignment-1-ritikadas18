bc60afe94a09bb2e5d172e4157c7b6970d803f47
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-02-24 22:54:07 +0530

    final commit

--- src/myContactList/myTasks.java
+++ src/myContactList/myTasks.java
@@ -73,4 +73,66 @@ public class myTasks {
         }
     }
 
+    public void viewContact(myLinkedList linkedList1) {
+        System.out.println("---Here are all your contacts---");
+
+        if (!linkedList1.isEmpty()) {
+
+            linkedList1.display();
+
+        } else {
+
+            System.out.println("NO results found");
+        }
+    }
+
+    public void deleteContact(myLinkedList linkedList1) {
+
+        if (!linkedList1.isEmpty()) {
+
+            System.out.println("Here are all the contacts: Press the number against" +
+
+                    " to delete the contact.");
+
+            linkedList1.displayOne();
+
+            int choice = sc.nextInt();
+
+            scanner.nextLine();
+
+            linkedList1.deleteNode(choice);
+
+            linkedList1.displayOne();
+
+            System.out.println("Deleted..!!");
+
+        } else {
+
+            System.out.println("No Contacts.You must need to add contacts Before you could delete..");
+
+        }
+
+    }
+
+
+    public void searchContact(myLinkedList linkedList1) {
+
+        System.out.println("You could search for a contact from their first names: ");
+
+        String firstName = sc.nextLine().trim();
+
+        if (linkedList1.indexOf(firstName) >= 0) {
+
+            System.out.println("Match found");
+
+            linkedList1.getNode(linkedList1.indexOf(firstName));
+
+        } else {
+
+            System.out.println("No match found");
+
+        }
+
+    }
+
 }
